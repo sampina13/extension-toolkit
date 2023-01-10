2103f12fb9f2c6c680a055a217c617d552e5721c
branchwelder <htwiggsmith@gmail.com>
2023-01-10 08:17:21 -0800

    options page

--- e-nator/options/options.js
+++ e-nator/options/options.js
@@ -1,59 +1,88 @@
-const defaultReplacements = {
+function listen(eventType, selector, callback) {
+  document.querySelector(selector).addEventListener(eventType, callback);
+}
+
+const rulesContainer = document.querySelector(".replacements");
+
+let rules = {
   e: "é",
   E: "É",
 };
 
-// Saves options to chrome.storage
-// function save_options() {
-//   var color = document.getElementById("color").value;
-//   var likesColor = document.getElementById("like").checked;
-//   chrome.storage.sync.set(
-//     {
-//       favoriteColor: color,
-//       likesColor: likesColor,
-//     },
-//     function () {
-//       // Update status to let user know options were saved.
-//       var status = document.getElementById("status");
-//       status.textContent = "Options saved.";
-//       setTimeout(function () {
-//         status.textContent = "";
-//       }, 750);
-//     }
-//   );
-// }
+function addRule(start, dest) {
+  return `<div class="rule">
+  <span class="start">${start}</span>
+  <span class="join">is converted to</span>
+  <span class="dest">${dest}</span>
+  <span class="remove" data-el=${start}>remove</span>
+</div>`;
+}
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get(
-    { replacements: defaultReplacements },
-    function (items) {
-      console.log(items);
-      for (const entry of Object.entries(items.replacements)) {
-        console.log(entry);
-      }
-      // document.getElementById("color").value = items.favoriteColor;
-      // document.getElementById("like").checked = items.likesColor;
+// Saves options to chrome.storage
+function saveRule(start, dest) {
+  rules[start] = dest;
+  chrome.storage.sync.set(
+    {
+      replacements: rules,
+    },
+    function () {
+      // Update status to let user know options were saved.
+      rulesContainer.innerHTML += addRule(start, dest);
+
+      console.log("added!");
     }
   );
 }
 
-// document.addEventListener("DOMContentLoaded", restore_options);
-// document.getElementById("save").addEventListener("click", save_options);
+function removeRule(start, dest) {
+  delete rules[start];
+  chrome.storage.sync.set(
+    {
+      replacements: rules,
+    },
+    function () {
+      // Update status to let user know options were saved.
+      // rulesContainer.innerHTML += addRule(start, dest);
 
-function addRule(rule) {
-  return `<div class="rule">
-  <span class="word">${rule[0]}</span>
-  <span class="join">is converted to</span>
-  <span class="word">${rule[1]}</span>
-  <span class="remove">remove</span>
-</div>`;
+      console.log("deleted!");
+    }
+  );
 }
 
-const replacements = document.querySelector(".replacements");
-
-for (const entry of Object.entries(defaultReplacements)) {
-  replacements.innerHTML += addRule(entry);
+function restoreRules() {
+  chrome.storage.sync.get({ replacements: rules }, function (items) {
+    rules = items.replacements;
+    for (const entry of Object.entries(items.replacements)) {
+      rulesContainer.innerHTML += addRule(entry[0], entry[1]);
+    }
+    listen("click", ".remove", (e) => {
+      console.log(e);
+    });
+  });
 }
+
+document.addEventListener("DOMContentLoaded", restoreRules);
+
+listen("click", "#add-rule", (e) => {
+  const start = document.querySelector("#rule-start");
+  const end = document.querySelector("#rule-dest");
+
+  saveRule(start.value, end.value);
+  start.value = "";
+  end.value = "";
+});
+
+function a() {
+  const divs = document.querySelectorAll(".remove");
+  console.log(divs);
+  divs.forEach((el) =>
+    el.addEventListener("click", (event) => {
+      console.log(event.target.getAttribute("data-el"));
+    })
+  );
+}
+
+true;
+
+// 13.1;
+// let hello = 13.2;
