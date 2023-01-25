4acccf3a9773f8cccfb5e81a0c87f193cf6441d4
spina13 <spina13@uw.edu>
2023-01-25 15:01:05 -0800

    test

--- my_chrome_extension/popup/popup.js
+++ my_chrome_extension/popup/popup.js
@@ -2,21 +2,25 @@ const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
 const button3 = document.getElementById("button3");
 
-// Add event listeners to the 3 buttons
-button1.addEventListener("click", (e) => updateContentScript(button1));
-button2.addEventListener("click", (e) => updateContentScript(button2));
-button3.addEventListener("click", (e) => updateContentScript(button3));
+async function fetchData(league) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'cf8bc36211mshe8ea1fa03e4a36bp1a2a82jsnb5ed8e946814',
+            'X-RapidAPI-Host': 'football98.p.rapidapi.com'
+        }
+    };
+    
+    const res = await fetch('https://football98.p.rapidapi.com/${league}/results', options)
+    const record = await res.json()
 
+    console.log('record', record)
+    
+    let myList = Object.values(record[0]);
 
-async function updateContentScript(buttonName) {
-  // Sends a message to the content script with an object that has the
-  // current value of the boolean (whether to get scores)
-  const message = { buttonName: buttonName };
-  const [tab] = await chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
-  });
-  const response = await chrome.tabs.sendMessage(tab.id, message);
-  // You can do something with response from the content script here
-  console.log(response);
+    document.getElementById("popup-content").innerHTML = myList[0].map(x => `<li>${x.awayTeam + ' ' + x.awayTeamScore + ' vs ' + x.homeTeam + ' ' + x.homeTeamScore}</li>`).join('');
 }
+
+button1.addEventListener("click", (e) => fetchData("premierleague"));
+button2.addEventListener("click", (e) => fetchData("laliga"));
+button3.addEventListener("click", (e) => fetchData("seriea"));
