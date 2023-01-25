2d3d9b3dec0dca0cfec2e32b2943018a03267a55
spina13 <spina13@uw.edu>
2023-01-25 15:53:47 -0800

    test

--- my_chrome_extension/popup/popup.js
+++ my_chrome_extension/popup/popup.js
@@ -2,13 +2,7 @@ const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
 const button3 = document.getElementById("button3");
 
-const leagueMappings = {
-    "button1": "premierleague",
-    "button2": "laliga",
-    "button3": "seriea"
-}
-
-async function fetchData(league) {
+async function fetchPrem() {
     const options = {
         method: 'GET',
         headers: {
@@ -17,15 +11,42 @@ async function fetchData(league) {
         }
     };
 
-    const res = await fetch('https://football98.p.rapidapi.com/${league}/results', options)
-    const record = await res.json()
-
-    console.log('record', record)
-
-    scores.innerHTML = record[0](x => `<li>${x.awayTeam + ' ' + x.awayTeamScore + ' vs ' + x.homeTeam + ' ' + x.homeTeamScore}</li>`).join('');
-
+    fetch('https://football98.p.rapidapi.com/premierleague/results', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
 }
 
-button1.addEventListener("click", (e) => fetchData(leagueMappings["button1"]));
-button2.addEventListener("click", (e) => fetchData(leagueMappings["button2"]));
-button3.addEventListener("click", (e) => fetchData(leagueMappings["button3"]));
+async function fetchlaLiga() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'cf8bc36211mshe8ea1fa03e4a36bp1a2a82jsnb5ed8e946814',
+            'X-RapidAPI-Host': 'football98.p.rapidapi.com'
+        }
+    };
+
+    fetch('https://football98.p.rapidapi.com/laliga/results', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
+
+async function fetchSeriea() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'cf8bc36211mshe8ea1fa03e4a36bp1a2a82jsnb5ed8e946814',
+            'X-RapidAPI-Host': 'football98.p.rapidapi.com'
+        }
+    };
+
+    fetch('https://football98.p.rapidapi.com/seriea/results', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
+
+button1.addEventListener("click", (e) => fetchPrem);
+button2.addEventListener("click", (e) => fetchlaLiga);
+button3.addEventListener("click", (e) => fetchSeriea);
