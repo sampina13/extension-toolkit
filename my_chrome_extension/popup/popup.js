d0ba37e750e9b0019c3da8aa61e32efabba67552
spina13 <spina13@uw.edu>
2023-01-25 15:15:11 -0800

    test

--- my_chrome_extension/popup/popup.js
+++ my_chrome_extension/popup/popup.js
@@ -2,6 +2,12 @@ const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
 const button3 = document.getElementById("button3");
 
+const leagueMappings = {
+    "button1": "premierleague",
+    "button2": "laliga",
+    "button3": "seriea"
+}
+
 async function fetchData(league) {
     const options = {
         method: 'GET',
@@ -10,17 +16,16 @@ async function fetchData(league) {
             'X-RapidAPI-Host': 'football98.p.rapidapi.com'
         }
     };
-    
+
     const res = await fetch('https://football98.p.rapidapi.com/${league}/results', options)
     const record = await res.json()
 
     console.log('record', record)
-    
-    let myList = Object.values(record[0]);
 
-    document.getElementById("popup-content").innerHTML = myList[0].map(x => `<li>${x.awayTeam + ' ' + x.awayTeamScore + ' vs ' + x.homeTeam + ' ' + x.homeTeamScore}</li>`).join('');
+    scores.innerHTML = record[0](x => `<li>${x.awayTeam + ' ' + x.awayTeamScore + ' vs ' + x.homeTeam + ' ' + x.homeTeamScore}</li>`).join('');
+
 }
 
-button1.addEventListener("click", (e) => fetchData("premierleague"));
-button2.addEventListener("click", (e) => fetchData("laliga"));
-button3.addEventListener("click", (e) => fetchData("seriea"));
+button1.addEventListener("click", (e) => fetchData(leagueMappings["button1"]));
+button2.addEventListener("click", (e) => fetchData(leagueMappings["button2"]));
+button3.addEventListener("click", (e) => fetchData(leagueMappings["button3"]));
